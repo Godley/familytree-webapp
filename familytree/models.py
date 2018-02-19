00c3a24b80076fae38844ea35f55cab0baa0d638
Godley <charlotte.godley@hotmail.co.uk>
2018-02-19 01:00:56 +0000

    feat: add partner to model representation

--- familytree/models.py
+++ familytree/models.py
@@ -5,6 +5,7 @@ class FamilyMember(models.Model):
     name = models.CharField(max_length=100)
     mother = models.ForeignKey('self', on_delete=models.CASCADE, related_name="mothered", null=True, blank=True)
     father = models.ForeignKey('self', on_delete=models.CASCADE, related_name="fathered", null=True, blank=True)
+    partner = models.OneToOneField('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
